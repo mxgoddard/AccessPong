7e7cc00a89be05a9bd32c5989a8bdaa3d60cc62d
Max Goddard <maxolivergoddard@gmail.com>
2019-04-09 14:02:32 +0100

    Updated sql script

--- data.sql
+++ data.sql
@@ -10,5 +10,26 @@ CREATE TABLE tbl_user
     id SERIAL PRIMARY KEY,
     firstName VARCHAR(50) NOT NULL,
     lastName VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL
+    email VARCHAR(50) NOT NULL,
+    pass VARCHAR(50) NOT NULL,
+    wins INT DEFAULT 0,
+    played INT DEFAULT 0
+);
+
+-- Create league table
+DROP TABLE IF EXISTS tbl_league;
+CREATE TABLE tbl_league
+(
+    id SERIAL PRIMARY KEY,
+    leagueName VARCHAR(50) NOT NULL,
+    amountPeople INT NOT NULL,
+    startDate DATE NOT NULL
+);
+
+-- Create league user link table
+DROP TABLE IF EXISTS tbl_leagueUserLink;
+CREATE TABLE tbl_leagueUserLink
+(
+    leagueId INT NOT NULL,
+    userId INT NOT NULL
 );
