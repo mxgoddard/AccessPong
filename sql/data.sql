3a77126704f04c61dcb53676ba7813fff79e30db
Max Goddard <maxolivergoddard@gmail.com>
2019-04-15 20:06:59 +0100

    Updated data.sql script with tbl_fixture info

--- sql/data.sql
+++ sql/data.sql
@@ -2,8 +2,12 @@
 
 \c db_access_pong
 
--- Clear user table
+-- Clear all tables in database
 DELETE FROM tbl_user;
+DELETE FROM tbl_league;
+DELETE FROM tbl_fixture;
+DELETE FROM tbl_leagueUserLink;
+
 
 -- Create 3 users
 INSERT INTO tbl_user
@@ -13,8 +17,6 @@ VALUES
 ('Ben', 'Foster', 'Ben.Foster@gmail.com', 'lkjhgfdsa'),
 ('Bob', 'Michaels', 'Bob.Michaels@yahoo.com', 'mnbvcxz');
 
--- Clear league table
-DELETE FROM tbl_league;
 
 -- Register league with those 3 users
 INSERT INTO tbl_league
@@ -22,9 +24,20 @@ INSERT INTO tbl_league
 VALUES
 ('Test League 1', 3, '05-08-2000');
 
+
+-- Create the league user link table
 INSERT INTO tbl_leagueUserLink
 (leagueId, userId)
 VALUES
 ('1', '1'),
 ('1', '2'),
 ('1', '3');
+
+
+-- Generate fixtures for those 3 players
+INSERT INTO tbl_fixture
+(id, leagueId, matchNum, playerOneId, playerTwoId, winnerId)
+VALUES
+(1, 1, 1, 2, 3, -1),
+(2, 1, 2, 1, 3, -1),
+(3, 1, 3, 1, 2, -1);
